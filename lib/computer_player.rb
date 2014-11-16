8dc952a69a59d3067719f7b1471351c14c962cea
cokuyan <chiryoku@gmail.com>
2014-11-15 20:00:48 -0500

    Create skeleton of player classes

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -1,4 +1,26 @@
 class ComputerPlayer
+  DICTIONARY = File.readlines('dictionary.txt').map(&:chomp)
 
+  def initialize
+
+  end
+
+  def pick_word
+  end
+
+  def word_length
+  end
+
+  def receive_length
+  end
+
+  def guess_letter
+  end
+
+  def confirm(guess)
+  end
+
+  def respond_to(guess, positions)
+  end
 
 end
